0eb1c3fb2d7b7572abb41a1a9050a8d80df130a1
Preston A. Elder <prez@neuromancy.net>
2005-04-06 03:42:15 +0000

    Fixed autogen to ensure Makefile.am and configure.ac are untouched.
    git-svn-id: http://svn.neuromancy.net/magick/trunk/Magick-III@1625 1d68a2a4-f605-0410-a2a0-863aa388aa54

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,11 @@
 export WANT_AUTOMAKE=1.6
 aclocal
 autoheader
+cp configure.ac configure.ac.save
+cp Makefile.am Makefile.am.save
 gettextize --copy
+mv configure.ac.save configure.ac
+mv Makefile.am.save Makefile.am
 libtoolize --automake --copy
 automake --add-missing --copy
 autoconf
