0fe5308592b2a1a6048f6383a6aaf9c47a519488
Preston A. Elder <prez@neuromancy.net>
2005-04-08 22:59:31 +0000

    Added stub files.
    git-svn-id: http://svn.neuromancy.net/magick/trunk/Magick-III@1640 1d68a2a4-f605-0410-a2a0-863aa388aa54

---
+++ src/committee.cpp
@@ -0,0 +1,39 @@
+#ifdef WIN32
+#pragma hdrstop
+#else
+#pragma implementation
+#endif
+
+/* Magick IRC Services
+**
+** (c) 2005 The Neuromancy Society <info@neuromancy.net>
+**
+** The above copywright may not be removed under any circumstances,
+** however it may be added to if any modifications are made to this
+** file.  All modified code must be clearly documented and labelled.
+**
+** This code is released under the GNU General Public License v2.0 or better.
+** The full text of this license should be contained in a file called
+** COPYING distributed with this code.  If this file does not exist,
+** it may be viewed here: http://www.neuromaancy.net/license/gpl.html
+**
+** ======================================================================= */
+#define RCSID(x,y) const char *rcsid_magick__committee_cpp_ ## x () { return y; }
+RCSID(magick__committee_cpp, "@(#)$Id$");
+
+/* ======================================================================= **
+**
+** For official changes (by The Neuromancy Society), please
+** check the ChangeLog* files that come with this distribution.
+**
+** Third Party Changes (please include e-mail address):
+**
+** N/A
+**
+** ======================================================================= */
+
+#include "magick.h"
+
+#include <mantra/core/trace.h>
+
+
